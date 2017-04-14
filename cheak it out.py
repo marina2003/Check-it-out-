fb34c7f6200813f8fdf815f9aba4eb016ac41123
David Brockway <twitter@pocketserver.co.uk>
2017-04-14 17:44:19 +0100

    Update cheak it out.py

--- cheak it out.py
+++ cheak it out.py
@@ -2,25 +2,45 @@
 # Cheack It Out 2017
 # Python 3.3.2/3.5.2
 
-# Welcome message to the user
-print("Welcome to loyalty card: Please follow the instructions provided")
+# Python Import
+import time
 
+# Welcome message 
+print("Welcome to Illuminate Jewelers")
+while True:
+    user = input("Would you like to cheack if your loyalty card is valid? (yes/no): ")
+    if user == 'yes':
+        break
+        print("Please wait while we process your request...")
+    else:
+        print("Exiting Program")
+        exit(0)
+time.sleep( 5 )
 # User Inputs
-#full_name = input("Please enter your full name: ")
-#postcode = input("Please enter your postcode: ")  
+fullName = input("Please Enter Your Name: ")
+postCode = input("Please Enter Your Postcode?")  
 cardNumber = input("Please enter your loyalty card 8 digit number: ")
-#CardExpiry = input("Please enter your cards expiry date: ")
+cardExpiry = input("Please enter your cards expiry date: ")
 
-# Testing Inputs
-# print('Cusomer Deatils', full_name, postcode, cardNumber)
+# Date Validation
 
 # Card Validation
 while len(cardNumber) != 8:
-    cardNumber = input('That was not a 8 digit number. Please enter your 8 cards digit number: ')
-    break
+  cardNumber = input('That was not a 8 digit number. Please enter your 8 cards digit number: ')
+  break
+print("Please wait while we process your information...")
 
 # Digit Functions
 check_Digit = int(cardNumber) // 10
 check_Digit = int(str(check_Digit)[::-1])
 print(check_Digit)
+
+# Customers - Loyalty Card Details Message
+print("Thank you, below you will find your customers details: ")
+time.sleep( 2 )
+print("Your FullName Is: ", fullName)
+print("Your Postcode Is: ", postCode)
+print("Your CardNumber Is: ", cardNumber)
+print("Your Card Expiry Date Is: ", cardExpiry)
+print("Thank-You for using our loyalty cards!")
 
