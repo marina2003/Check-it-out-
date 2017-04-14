16c6ce98de76fd3f179186f8e530eeb7c8c5573a
David Brockway <twitter@pocketserver.co.uk>
2017-04-14 23:50:04 +0100

    Update cheak it out.py

--- cheak it out.py
+++ cheak it out.py
@@ -15,18 +15,17 @@ while True:
     else:
         print("Exiting Program")
         exit(0)
-time.sleep( 5 )
+time.sleep( 0 )
 # User Inputs
-fullName = input("Please Enter Your Full Name: ")
-time.sleep( 1 )
-postCode = input("Please Enter Your Postcode?")
-time.sleep( 1 )
-print("Just Some More Details We Need To Process Your Request...")
-time.sleep( 1 )
+#fullName = input("Please Enter Your Full Name: ")
+#time.sleep( 1 )
+#postCode = input("Please Enter Your Postcode?")
+#time.sleep( 1 )
+#print("Just Some More Details Before We Can Process Your Request...")
+#time.sleep( 1 )
 cardNumber = input("Please Enter Your Loyalty Card Eight Digit Card Number: ")
-cardExpiry = input("Please Enter Your Cards Expiry Date: ")
 
-# Date Validation
+# CardExpiry Date Validation
 
 # Card Validation
 while len(cardNumber) != 8:
@@ -34,11 +33,47 @@ while len(cardNumber) != 8:
   break
 print("Please Wait While We Process Your Information...")
 
-# Digit Functions
-check_Digit = int(cardNumber) // 10
-check_Digit = int(str(check_Digit)[::-1])
+# Digit Conversation
+number_list = list(map(int, cardNumber))
+
+# Store Eight Digit
+check_Digit = number_list[7] #grab the 8th item
 print(check_Digit)
-                           
+#check_Digit = list(map(int, cardNumber))[7]
+
+# Digit Function Removes Eight Digit
+del(number_list[7])
+
+# Digit conversation list to string
+string_number = ''.join(map(str, number_list))
+
+# Digit Reverse Functions
+reversed_number = int(string_number[::-1] )
+print(reversed_number)
+time.sleep( 2 )
+
+# Digit Call Function
+Digits = reversed_number
+# Digit Extraction Functions
+Digit1 = int(str(Digits)[0])
+Digit3 = int(str(Digits)[2])
+Digit5 = int(str(Digits)[4])
+Digit7 = int(str(Digits)[6])
+# Loop over all digits
+for digit in str(Digits):
+# Convert current digit to an integer
+  digit = int(digit)
+# Odd Digit Multiply Functions
+  if digit % 2 == 1:
+    digit = digit * 2
+    print(digit)
+# Digit Function If Greated Than 9 Then -9
+  if digit > 9:
+    digit = digit - 9
+    print(digit)
+# Digit Function Adds Up All Digits
+total = sum(list(map(int, Digits)))
+print(total)
 
 # Customers - Loyalty Card Details Message
 print("Thank You, Below You Will Find Your Customers Details: ")
