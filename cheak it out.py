f1bbd8cbb2c5c8d337dd57eaa258259e295c7cdf
David Brockway <twitter@pocketserver.co.uk>
2017-04-14 14:04:10 +0100

    Create cheak it out.py

---
+++ cheak it out.py
@@ -0,0 +1,14 @@
+# WJEC Controlled Asessment Computer Science
+# Cheack It Out 2017
+# Python 3.3.2/3.5.2
+
+# Welcome message to the user
+print("Welcome to loyalty card: Please follow the instructions provided")
+
+# User Inputs
+#full_name = input("Please enter your full name: ")
+#postcode = input("Please enter your postcode: ")  
+cardNumber = input("Please enter your loyalty card 8 digit number: ")
+#CardExpiry = input("Please enter your cards expiry date: ")
+
+
