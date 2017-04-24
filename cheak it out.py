c41caf5a0a905af5446e25fcd04ef85917f679c0
David Brockway <twitter@pocketserver.co.uk>
2017-04-24 19:37:57 +0100

    Update cheak it out.py

--- cheak it out.py
+++ cheak it out.py
@@ -2,10 +2,10 @@
 # Cheack It Out 2017
 # Python 3.3.2/3.5.2
 
-# Python Import
+########### Python Imports ###########
 import time
 
-# Welcome Message
+########### Welcome Function ###########
 print("Welcome to Illuminate Jewelers Loyalty Card System") 
 while True:
     user = input("Would You Like To Check If Your Loyalty Card Is Valid? (yes/no): ")
@@ -16,18 +16,21 @@ while True:
         print("Exiting Program")
         exit(0)
 time.sleep( 0 )
-# User Inputs
-#fullName = input("Please Enter Your Full Name: ")
-#time.sleep( 1 )
-#postCode = input("Please Enter Your Postcode?")
-#time.sleep( 1 )
-#print("Just Some More Details Before We Can Process Your Request...")
-#time.sleep( 1 )
-cardExpiry = input("Please enter your cards expiry date: ")
-#time.sleep ( 1 )
-cardNumber = input("Please enter your loyalty card eight digit number: ")
 
-# CardExpiry Date Validation
+########### User's Inputs ###########
+
+fullName = input("Please Enter Your Full Name: ")
+time.sleep( 1 )
+postCode = input("Please Enter Your Postcode?")
+time.sleep( 1 )
+print("Just Some More Details Before We Can Process Your Request...")
+time.sleep( 1 )
+cardExpiry = input("Please enter your cards expiry date: ")
+time.sleep ( 1 )
+cardNumber = input("Please enter your loyalty card eight digit ")
+
+########### CardExpiry Date Validation ###########
+
 print (time.strftime("%d/%m/%Y"))# Gets Current date
 currentDate = (time.strftime("%d/%m/%Y"))
 
@@ -38,49 +41,49 @@ dayExpiry = cardExpiry[0:2]
 NewcardExpiry = yearExpiry + "/" + monthExpiry + "/" + dayExpiry
 
 if NewcardExpiry < currentDate:
-    print("card has expired")
+    print("Card has reached its expiry date")
 
+########### Card Validation ###########
 
-# Card Validation
 while len(cardNumber) != 8:
   cardNumber = input('Invalid not an eight digit number. Please enter your cards eight digit number: ')
   break
 print("Please wait while we process your information...")
 
-# Digit Conversation
+########### Digit Conversation ########### 
 numberList = list(map(int, cardNumber))
 
-# Store Eight Digit
+########### Store Eight Digit ###########
 checkDigit = numberList[7] #grab the 8th item
 print(checkDigit)
 checkDigit = list(map(int, cardNumber))[7]
 
-# Digit Function Removes Eight Digit
+########### Digit Function Removes Eight Digit ###########
 del(numberList[7])
 
-# Digit conversation list to string
+########### Digit Conversation list to a string ###########
 stringNumber = ''.join(map(str, numberList))
 
-# Digit Reverse Functions
+########### Digit Reverse Function ########### 
 reversedNumber = int(stringNumber[::-1] )
 print(reversedNumber)
 
-# Digit Call Function
+########### Digit Call Function ########### 
 digits = reversedNumber
-# Digit Extraction Functions
+########### Digit Extraction Functions ########### 
 digit1 = int(str(digits)[0])
 digit3 = int(str(digits)[2])
 digit5 = int(str(digits)[4])
 digit7 = int(str(digits)[6])
-# Loop over all digits
+###########  Loop over all digits ########### 
 for digit in str(digits):
-# Convert current digit to an integer
+########### Convert current digit to an integer ########### 
   digit = int(digit)
-# Odd Digit Multiply Functions
+########### Odd Digit Multiply Functions ########### 
   if digit % 2 == 1:
     digit = digit * 2
     print(digit)
-# Digit Function If Greater Than 9 Then -9
+########### Digit Function If Greater Than 9 Then -9 ########### 
   if digit > 9:
     digit = digit - 9
     print(digit)
@@ -88,7 +91,7 @@ for digit in str(digits):
 total_digits = digit1 + digit3 + digit5 + digit7 + digit
 print(total_digits)
 
-# Customers - Loyalty Card Details Message
+########### Customers - Loyalty Card Details Message ###########
 print("Thank You, Below You Will Find Your Customers Details: ")
 time.sleep( 2 )
 print("Your FullName Is: ", fullName)
@@ -100,4 +103,5 @@ time.sleep( 1 )
 print("Your Card Expiry Date Is: ", cardExpiry)
 time.sleep( 1 )
 print("Thank You For Using Our Loyalty Cards! GoodBye")
+
 
